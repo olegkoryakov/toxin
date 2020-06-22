496536fc9c4ca62a45a83cc5b504b30a0ee43f46
Oleg Koryakov <outflowpwnz@gmail.com>
2020-06-22 15:30:08 +0700

    connect dropdown scripts to app

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,4 +1,5 @@
 import LikeButton from '../blocks/like-button/ts/LikeButton';
+import JsDropdown from '../blocks/js-dropdown/ts/JsDropdown';
 
 const likeButtons = document.querySelectorAll('.like-button');
 const likeButtonsList = [];
@@ -6,5 +7,15 @@ const likeButtonsList = [];
 likeButtons.forEach((element) => {
   if (element instanceof HTMLElement) {
     likeButtonsList.push(new LikeButton(element));
+  }
+});
+
+const jsDropdowns = document.querySelectorAll('.js-dropdown');
+const jsDropdownsList = [];
+
+jsDropdowns.forEach((element) => {
+  const buttons = Array.from(element.querySelectorAll('.js-dropdown__button'));
+  if (element !== undefined && buttons !== undefined) {
+    jsDropdownsList.push(new JsDropdown((element as HTMLElement), ...(buttons as HTMLElement[])));
   }
 });
